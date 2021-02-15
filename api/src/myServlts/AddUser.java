99b01abc28e13563c319bd649b054b19b3979969
wangsy <1101008421@qq.com>
2021-02-15 19:10:09 +0800

    用户信息不完善，不允许添加

--- api/src/myServlts/AddUser.java
+++ api/src/myServlts/AddUser.java
@@ -51,6 +51,8 @@ public class AddUser extends HttpServlet {
 		
 
 		User user = new User();
+		request.setCharacterEncoding("utf-8");
+		
 
 		response.setHeader("Content-Type", "text/html;charset=UTF-8");
 		String userName = request.getParameter("userName");
@@ -59,28 +61,87 @@ public class AddUser extends HttpServlet {
 		String adress = request.getParameter("adress");
 		String tel = request.getParameter("tel");
 		long time = System.currentTimeMillis();
-		System.out.println(userName);
+	
 		
+		if(userName!=null&&age!=null&&gender!=null&&adress!=null&&tel!=null) {
+			
+			try {
+				user.addUser(userName, age, gender, adress, tel, time);
 
-		try {
-			user.addUser(userName, age, gender, adress, tel, time);
+				JSONObject res = new JSONObject();
+				res.put("status", 200);
+				res.put("success", true);
+				res.put("desc", "添加成功");
+				response.getWriter().append(res.toString());
+				
+				
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			
+		}
+		
+		
+		
+		else{
+			
+			
 			JSONObject res = new JSONObject();
 			res.put("status", 200);
-			res.put("success", true);
-			res.put("desc", "添加成功");
+			res.put("success", false);
+			res.put("desc", "添加失败,请完善用户信息！");
 			response.getWriter().append(res.toString());
+			
+			
+		}
+		
+		
+		
+		
+			
+		/*	try {
+			
+				
+				if(userName!=null&&age!=null&&gender!=null&&adress!=null&&tel!=null) {
+				user.addUser(userName, age, gender, adress, tel, time);
+				
+				
+				JSONObject res = new JSONObject();
+				res.put("status", 200);
+				res.put("success", true);
+				res.put("desc", "添加成功");
+				response.getWriter().append(res.toString());
 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+				}
+			
+				
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				JSONObject res = new JSONObject();
+				res.put("status", 200);
+				res.put("success", false);
+				res.put("desc", "添加失败");
+				response.getWriter().append(res.toString());
+			
+			}
+			
+		
+		}else{// TODO Auto-generated catch block
 			JSONObject res = new JSONObject();
 			res.put("status", 200);
 			res.put("success", false);
 			res.put("desc", "添加失败");
 			response.getWriter().append(res.toString());
-			e.printStackTrace();
+		
 
 		}
 
-	}
+	*/
 
+}
 }
