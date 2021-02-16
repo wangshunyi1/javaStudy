ef757cd01d532dc8dcaada89cf6bb8fc4ebfb3f0
wangsy <1101008421@qq.com>
2021-02-16 15:49:30 +0800

    注册账户

--- api/src/com/wsy/User.java
+++ api/src/com/wsy/User.java
@@ -1,4 +1,5 @@
 package com.wsy;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,39 +9,40 @@ import java.util.ArrayList;
 import net.sf.json.JSONObject;
 
 public class User {
-	
-	DBConnection DBConn = new DBConnection();                      // 寮曞叆杩炴帴鏁版嵁搴撴柟娉曠被
-	Function Fun = new Function();                                  // 寮曞叆鏁版嵁澶勭悊鏂规硶绫�
-	//鏂伴椈鍒楄〃鏌ヨ鏂规硶
+
+	DBConnection DBConn = new DBConnection(); // 寮曞叆杩炴帴鏁版嵁搴撴柟娉曠被
+	Function Fun = new Function(); // 寮曞叆鏁版嵁澶勭悊鏂规硶绫�
+	// 鏂伴椈鍒楄〃鏌ヨ鏂规硶
+
 	public String getUserList() {
-		
-		try {                                                        // 鐢ㄤ簬鑾峰彇绯荤粺杩愯寮傚父淇℃伅
-		
-			Connection Conn = DBConn.getConn();                  // 寤虹珛鏁版嵁搴撹繛鎺�
-			Statement stmt = Conn.createStatement();            // 鍒涘缓鏁版嵁鏌ヨ
-			ResultSet rs = null;                                  // 瀹氫箟缁撴灉闆�			
-			
+
+		try { // 鐢ㄤ簬鑾峰彇绯荤粺杩愯寮傚父淇℃伅
+
+			Connection Conn = DBConn.getConn(); // 寤虹珛鏁版嵁搴撹繛鎺�
+			Statement stmt = Conn.createStatement(); // 鍒涘缓鏁版嵁鏌ヨ
+			ResultSet rs = null; // 瀹氫箟缁撴灉闆�
+
 			// 鍒涘缓sql璇彞鏌ヨNews琛ㄥ叏閮ㄤ俊鎭�
 			String sSql = "select * from userList order by id DESC";
 			// 閫氳繃鎵цsql璇彞寰楀埌鏌ヨ缁撴灉
 			rs = stmt.executeQuery(sSql);
-			
+
 			JSONObject res = new JSONObject();
-			Boolean flag=rs.next(); 
-			
-			if (!flag) {                                     // 鍒ゅ畾褰撴煡璇㈢粨鏋滀负绌�
+			Boolean flag = rs.next();
+
+			if (!flag) { // 鍒ゅ畾褰撴煡璇㈢粨鏋滀负绌�
 				res.put("status", 200);
 				res.put("desc", "请求成功！");
 				res.put("data", null);
 				res.put("success", false);
-			} else {                                               // 鍒ゅ畾褰撴煡璇㈢粨鏋滄槸鍚︿负绌�
-				
+			} else { // 鍒ゅ畾褰撴煡璇㈢粨鏋滄槸鍚︿负绌�
+
 				res.put("status", 200);
 				res.put("desc", "请求成功！");
-			
+
 				res.put("success", true);
 				ArrayList arr = new ArrayList();
-				while(flag) {
+				while (flag) {
 					JSONObject obj = new JSONObject();
 					obj.put("id", rs.getInt("id"));
 					obj.put("userName", rs.getString("userName"));
@@ -49,99 +51,146 @@ public class User {
 					obj.put("adress", rs.getString("adress"));
 					obj.put("tel", rs.getString("tel"));
 					obj.put("time", rs.getString("time"));
-					arr.add(obj);                                  // 鍒ゅ畾鏄惁瀛樺湪涓嬩竴鏉′俊鎭�
-					flag=rs.next();
+					arr.add(obj); // 鍒ゅ畾鏄惁瀛樺湪涓嬩竴鏉′俊鎭�
+					flag = rs.next();
 				}
 				res.put("data", arr);
 			}
-			rs.close();                                          // 鍏抽棴缁撴灉闆�
-			stmt.close();                                       // 鍏抽棴鏌ヨ
-			Conn.close();                                       // 鍏抽棴鏁版嵁杩炴帴
-			return res.toString();                              // 杩斿洖瀛楃涓叉暟鎹�
-		} catch (Exception e) {                                // 寰楀埌绯荤粺杩愯寮傚父
-			System.out.println(e);
-			return "No";                                       // 濡傛灉绯荤粺寮傚父鏂规硶杩斿洖瀛楃鈥淣o鈥�
+			rs.close(); // 鍏抽棴缁撴灉闆�
+			stmt.close(); // 鍏抽棴鏌ヨ
+			Conn.close(); // 鍏抽棴鏁版嵁杩炴帴
+			return res.toString(); // 杩斿洖瀛楃涓叉暟鎹�
+		} catch (Exception e) { // 寰楀埌绯荤粺杩愯寮傚父
+			
+			return "No"; // 濡傛灉绯荤粺寮傚父鏂规硶杩斿洖瀛楃鈥淣o鈥�
 		}
 	}
-	public void addUser(String userName,String age,String gender,String adress,String tel,long time) throws SQLException {
-			Connection Conn = DBConn.getConn();
-			Statement stmt = Conn.createStatement();
-			
-			String sSql="INSERT INTO userList (userName, gender, age, adress, tel, time) values ('"+ userName + "','" + gender + "'," + age + ",'" + adress +  "'," +tel + ","+ time + ")";
-			int rs = stmt.executeUpdate(sSql);
-			
-			System.out.print(rs);
-			
+
+	public void addUser(String userName, String age, String gender, String adress, String tel, long time)
+			throws SQLException {
+		Connection Conn = DBConn.getConn();
+		Statement stmt = Conn.createStatement();
+
+		String sSql = "INSERT INTO userList (userName, gender, age, adress, tel, time) values ('" + userName + "','"
+				+ gender + "'," + age + ",'" + adress + "'," + tel + "," + time + ")";
+		int rs = stmt.executeUpdate(sSql);
+
+		System.out.print(rs);
+
 	}
 
-	public void deleteUser(String id) throws SQLException  {
-			
+	public void deleteUser(String id) throws SQLException {
+
 		Connection Conn = DBConn.getConn();
-	
+
 		Statement stmt = Conn.createStatement();
-		
-		
-	
-		
-		
-		String sSql="delete from userList where id = '"+id+"'";
+
+		String sSql = "delete from userList where id = '" + id + "'";
 		stmt.execute(sSql);
-			
-		
-		
-		}
-	
-	
-	
-	public void updateUser(String id,String userName,String  age,String  gender, String adress, String tel) throws SQLException {
-		
-		Connection Conn = DBConn.getConn();
-		
-		Statement stmt = Conn.createStatement();
-		//String s = null;
-	//	String sSql = "update userList set "+s+"where id = "+id;
-		//判断userName用户名是否更新
-		if(userName!=null) {
-			String sSql = "update userList set userName = '"+userName+"' where id = "+id+"";
-			System.out.println(sSql);
-			stmt. execute(sSql);
-			
-		}
-		//判断age是否更新
-		if(age!=null) {
-			String sSql = "update userList set age = '"+age+"' where id = "+id+"";
-			System.out.println(sSql);
-			stmt. execute(sSql);
-		}	
-		//判断gender是否更新
-		if(gender!=null) {
-			String sSql = "update userList set gender = '"+gender+"' where id = "+id+"";
-			System.out.println(sSql);
-			stmt. execute(sSql);
-		
-		}	
-		//判断adress是否更新
-		if(adress!=null) {
-			String sSql = "update userList set adress = '"+adress+"' where id = "+id+"";
-			System.out.println(sSql);
-			stmt. execute(sSql);
-			
-		}
-		//判断tel是否更新
-		if(tel!=null) {
-			String sSql = "update userList set tel = '"+tel+"' where id = "+id+"";
-			System.out.println(sSql);
-			stmt. execute(sSql);
-			
-		}
-		
+
 	}
+
+	public void updateUser(String id, String userName, String age, String gender, String adress, String tel)
+			throws SQLException {
+
+		Connection Conn = DBConn.getConn();
+
+		Statement stmt = Conn.createStatement();
+		// String s = null;
+		// String sSql = "update userList set "+s+"where id = "+id;
+		// 判断userName用户名是否更新
+		if (userName != null) {
+			String sSql = "update userList set userName = '" + userName + "' where id = " + id + "";
+			System.out.println(sSql);
+			stmt.execute(sSql);
+
+		}
+		// 判断age是否更新
+		if (age != null) {
+			String sSql = "update userList set age = '" + age + "' where id = " + id + "";
+			System.out.println(sSql);
+			stmt.execute(sSql);
+		}
+		// 判断gender是否更新
+		if (gender != null) {
+			String sSql = "update userList set gender = '" + gender + "' where id = " + id + "";
+			System.out.println(sSql);
+			stmt.execute(sSql);
+
+		}
+		// 判断adress是否更新
+		if (adress != null) {
+			String sSql = "update userList set adress = '" + adress + "' where id = " + id + "";
+			System.out.println(sSql);
+			stmt.execute(sSql);
+
+		}
+		// 判断tel是否更新
+		if (tel != null) {
+			String sSql = "update userList set tel = '" + tel + "' where id = " + id + "";
+			System.out.println(sSql);
+			stmt.execute(sSql);
+
+		}
+
+	}
+
+	public void login(String iden, String password) throws SQLException {
+		Connection Conn = DBConn.getConn();
+
+		Statement stmt = Conn.createStatement();
+		String sSql = "select iden from login ";
+		ResultSet rs = stmt.executeQuery(sSql);
+		JSONObject res = new JSONObject();
+		boolean b=true;
+		while(rs.next()) {
+			
+			if(rs.getString("iden").equals(iden)) {
+			
+			/*	res.put("status", 200);
+				res.put("desc", "请求成功！");
+				res.put("data", null);
+				res.put("success", "账户已存在");
+				*/
+				b=false;
+				break;
+				
+			}
+			
+	}
+		System.out.println(b);
 		
-	
-	
+		if(b) {
+			String sSql1 = "INSERT INTO login (iden, password) values ('" + iden + "','"
+					+ password + "')";
+		
+			System.out.println(sSql1);
+			System.out.println("添加成功");
+				stmt.executeUpdate(sSql1);
+			
+		}else {
+			
+			System.out.println("账户存在");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		rs.close();
+		stmt.close();
+		Conn.close(); 
 	
 
+	}
 
-	
-	
 }
---
+++ api/src/myServlts/Login.java
@@ -0,0 +1,63 @@
+package myServlts;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.wsy.User;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		User user = new User();
+		request.setCharacterEncoding("utf-8");
+	
+		response.setHeader("Content-Type", "text/html;charset=UTF-8");
+		
+		String iden = request.getParameter("iden");
+		String password = request.getParameter("password");
+		
+		try {
+			user.login(iden,password);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+	}
+
+}
